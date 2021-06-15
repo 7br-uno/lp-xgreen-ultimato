cc4f601af2c1e451480126bab963e442d2bb54db
Bruno Neves <dev@7br.uno>
2021-06-15 20:57:33 -0300

    Page episodio.vue

---
+++ components/episodio/mainBaner.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="black">
+    <v-container class="px-8 px-sm-16 py-1">
+      <div class="banner">
+        <header class="white--text d-flex justify-center">
+          <img src="/ultimato/img/logo300x300_fundotransparente.png" />
+        </header>
+        <img src="/ultimato/img/bg_home_monstros.png" />
+      </div>
+    </v-container>
+  </div>
+</template>
+<style>
+.banner {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+</style>
---
+++ pages/_episodio.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <main-baner />
+  </div>
+</template>
+<script>
+import mainBaner from "~/components/episodio/mainBaner";
+export default {
+  components:{
+    mainBaner
+  },
+  mounted() {
+    console.log(this.$route.params.episodio);
+  }
+}
+</script>
