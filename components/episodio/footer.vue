929c04cbb2c090cc42458f27910c2eab5d6d1db0
Daniel Ribeiro <academicodan@gmail.com>
2021-06-16 15:05:26 -0300

    components of the episodes page

---
+++ components/episodio/footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="accent py-16">
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="2" class="d-flex justify-center">
+          <div class="d-flex align-center">
+            <img src="/ultimato/img/logo_xg.png" alt="logo x-green"/>
+          </div>
+        </v-col>
+        <v-col cols="12" offset-sm="1" sm="9" class="white--text">
+          <p class="font text-justify">
+            Resultados passados não são garantias de resultadosfuturos. As
+            informaçõesdeste site destinam-se ao seu conhecimento geral enão
+            garantimos resultadosespecíficos e valores exatos de lucro. Os
+            resultadossão de inteiraresponsabilidade do assinante e/ou aluno que
+            realizaas suas própriasoperações.O nosso conteúdo tem fins de
+            conhecimento e cultura.São voltados àintrodução aos conhecimentos em
+            educação e mercadosfinanceiros, dentrodos melhores valores éticos e
+            morais, com o objetivode promover ashabilidades necessárias para o
+            correto entendimentoem educação financeira.
+          </p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+<style lang="scss" scoped>
+.font {
+  font-size: 12px;
+}
+</style>
--- pages/_episodio.vue
+++ pages/_episodio.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
     <main-baner />
+    <special-offer />
+    <official-telegran />
+    <my-footer />
   </div>
 </template>
 <script>
 import mainBaner from "~/components/episodio/mainBaner";
+import officialTelegran from "~/components/episodio/officialTelegran.vue"
+import specialOffer from "~/components/episodio/specialOffer.vue"
+import myFooter from "~/components/episodio/footer.vue"
 export default {
   components:{
-    mainBaner
+    mainBaner,
+    officialTelegran,
+    specialOffer,
+    myFooter
   },
   mounted() {
     console.log(this.$route.params.episodio);
